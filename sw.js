22d4c53750c0df2c7c0a3de607da7a6937ed106f
vokessammi-arch <vokessammi@gmail.com>
2026-09-03 16:44:48 -0700

    Update service worker for Tend refresh button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pcc-v11';
-const ASSETS=['./','./index.html','./manifest.json','./enhancements.js','./enhancements.css'];
+const CACHE='pcc-v12';
+const ASSETS=['./','./index.html','./manifest.json','./enhancements.js','./enhancements.css','./bugfixes.js','./refresh.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
