1cb70fd7e9d6a64ea966d5253a19210196fa707c
vokessammi-arch <vokessammi@gmail.com>
2026-09-03 19:32:48 -0700

    Push Tend v13 updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pcc-v12';
+const CACHE='pcc-v13';
 const ASSETS=['./','./index.html','./manifest.json','./enhancements.js','./enhancements.css','./bugfixes.js','./refresh.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
